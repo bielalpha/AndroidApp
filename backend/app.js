03474a88938accff302af34211e17ee8a9c1ebb4
Ideyas-Gabriel <gabrielaraujo@ideyas.it>
2020-12-30 16:38:01 -0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+const morgan = require('morgan')
+
+const accountRouter = require('./routes/conta')
+
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use((req,res, next)=>{
+    res.header('Acces-Control-Allow-Origin', '*')
+    res.header(
+        'Acces-Control-Allow-Header', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    if(req.method === 'OPTIONS'){
+        res.header('Acces-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+        return res.status(200).send({})
+    }
+    next()
+})
+
+app.use('/conta', accountRouter)
+
+app.use((req,res, next)=>{
+    const erro = new Error('Route not found')
+    erro.status= 404
+    next(erro)
+});
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500)
+    return res.send({
+        erro:{
+            message: error.message
+        }
+    })
+})
+
+
+
+module.exports = app;
